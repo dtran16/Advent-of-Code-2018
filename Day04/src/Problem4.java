00e76b12b0662703ed5c43dc0ce845bf490331f1
Dylan Tran <dylantran@berkeley.edu>
2018-12-03 17:14:41 -0800

    add day04

---
+++ Day04/src/Problem4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class Problem4 {
+    public static void main(String[] args) throws IOException{
+        Scanner in = new Scanner(new File("input.txt"));
+
+        while (in.hasNext()) {
+
+        }
+    }
+
+}
