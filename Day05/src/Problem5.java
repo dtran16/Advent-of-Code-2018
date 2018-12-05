9b250fcacf75a5a24b3bbf2a7f27c2c451614167
Dylan Tran <dylantran@berkeley.edu>
2018-12-04 20:22:14 -0800

    add day05

---
+++ Day05/src/Problem5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+
+public class Problem5 {
+    public static void main (String[] args) throws IOException{
+        Scanner in = new Scanner(new File("input.txt"));
+
+        while (in.hasNext()) {
+            
+        }
+
+    }
+
+}
