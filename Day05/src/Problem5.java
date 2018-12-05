7858f2d06bbdd34dadcdd96706a5ab0e22cbbbe5
Dylan Tran <dylantran@berkeley.edu>
2018-12-04 22:01:04 -0800

    improved runtime of part1

--- Day05/src/Problem5.java
+++ Day05/src/Problem5.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.File;
-import java.util.ArrayList;
 
 public class Problem5 {
     public static void main (String[] args) throws IOException{
         Scanner in = new Scanner(new File("input.txt"));
         String poly = in.nextLine();
         System.out.println(findLength(poly)); //part 1
+        System.out.println(findSmallest(poly)); //part 2 answer
+    }
+    public static int findLength (String poly) {
+        for (int i = 1; i < poly.length() - 1;) {
+            boolean diff1 = Math.abs(poly.charAt(i - 1) - poly.charAt(i)) == 32;
+            boolean diff2 = Math.abs(poly.charAt(i + 1) - poly.charAt(i)) == 32; // if same letter, then uppercase - lowercase = 32
+            if (diff1) {
+                poly = poly.substring(0, i - 1) + poly.substring(i + 1);
+                if (i > 1) {
+                    i -= 1;
+                }
+            } else if (diff2){
+                poly = poly.substring(0, i) + poly.substring(i + 2);
+            } else {
+                i += 1;
+            }
+        }
+        return poly.length();
+    }
+    public static int findSmallest (String poly) {
         int min = Integer.MAX_VALUE;
         for (int i = 65; i <= 90; i += 1) { //used ascii values
             String exp = "";
@@ -23,26 +42,6 @@ public class Problem5 {
                 min = ln;
             }
         }
-        System.out.println(min); //part 2 answer
-    }
-    public static int findLength (String poly) {
-        boolean normal = false;
-        while (!normal) {
-            normal = true;
-            for (int i = 1; i < poly.length() - 1;) {
-                boolean diff1 = Math.abs(poly.charAt(i - 1) - poly.charAt(i)) == 32;
-                boolean diff2 = Math.abs(poly.charAt(i + 1) - poly.charAt(i)) == 32; // if same letter, then uppercase - lowercase = 32
-                if (diff1) {
-                    poly = poly.substring(0, i - 1) + poly.substring(i + 1);
-                    normal = false;
-                } else if (diff2){
-                    poly = poly.substring(0, i) + poly.substring(i + 2);
-                    normal = false;
-                } else {
-                    i += 1;
-                }
-            }
-        }
-        return poly.length();
+        return min;
     }
 }
