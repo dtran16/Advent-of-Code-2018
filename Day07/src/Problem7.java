2db1e95c734a0d60ef42f6750c7d1b6c9995b460
Dylan Tran <dylantran@berkeley.edu>
2018-12-07 02:53:13 -0800

    clean up part1

--- Day07/src/Problem7.java
+++ Day07/src/Problem7.java
@@ -8,69 +8,55 @@ import java.util.Queue;
 
 public class Problem7 {
     static int[][] adj = new int[26][27]; //adjacency matrix representing graph, last column of each array
-                                        // represents whether it has a predecessor (1 if does, 0 otherwise, 2 if processed)
-    static Queue<Integer> q;
+                                        // represents count of predecessors ( -1 if processed)
+    static Queue<Integer> q = new PriorityQueue<>(); //processed in alphabetical order
+    static int[] workers = new int[5];
+    static int[] letters = new int[5]; //letters that they are working on
+    int time = 0;
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(new File("input.txt"));
         Pattern p = Pattern.compile("Step (\\w) must be finished before step (\\w) can begin.");
         Matcher m;
-        //read in input
-        while(in.hasNextLine()) {
+        while(in.hasNextLine()) { //read in input
             String line = in.nextLine();
             m = p.matcher(line);
             if(m.find()) {
                 char from = m.group(1).charAt(0);
                 char to = m.group(2).charAt(0);
                 adj[to - 'A'][from - 'A'] = 1; //adding edges to graph [to][from] = 1 if there is an edge to --> from
-                adj[to - 'A'][26] = 1;
+                adj[to - 'A'][26] += 1;
             }
         }
         in.close();
-        q = new PriorityQueue<>(); //processed in alphabetical order
-        for (int i = 0; i < 26; i += 1) { //search for steps with no prereqs
-            if (adj[i][26] == 0) {
-                q.add(i);
-                adj[i][26] = 2;
-            }
-        }
         String res = bfs();
         System.out.println(res); //part 1
 
-        int[] workers = new int[5];
-
     }
-    //breadth first search
+    //breadth first search - sort of - alphabetical order prioritized
     public static String bfs() {
+        for (int i = 0; i < 26; i += 1) { //search for steps with no prereqs, initializes queue
+            if (adj[i][26] == 0) {
+                q.add(i);
+            }
+        }
         String res = "";
-        while (!q.isEmpty()) {
+        while (!q.isEmpty()) { //performs search
             int a = q.remove();
-            if (avail(a)) {
+            if (adj[a][26] == 0) { // has no more prerequisites
                 res += (char) (a + 'A');
                 addSuccessors(a);
             }
         }
         return res;
     }
-    private static void addSuccessors(int a) { //adds successors and removes the edges (removes predecessors)
+    private static void addSuccessors(int a) { //adds successors and removes the from to successors
         for (int i = 0; i < 26; i += 1) {
             if (adj[i][a] == 1) {
                 q.add(i);
                 adj[i][a] = 0;
+                adj[i][26] -= 1;
             }
         }
-        adj[a][26] = 0;
-    }
-    private static boolean avail(int a) { //checks if this step is ready to be done (has no more predecessors
-        if (adj[a][26] == 2) {
-            return true;
-        } else if (adj[a][26] == 0) {
-            return false;
-        }
-        for (int i = 0; i < 26; i += 1) {
-            if (adj[a][i] == 1) {
-                return false;
-            }
-        }
-        return true;
+        adj[a][26] = -1;
     }
 }
